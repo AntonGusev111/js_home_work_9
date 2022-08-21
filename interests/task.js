096409889bc86777c473369fb6b3c40948d11bdd
GusevADresume <resumeGusevAD@gmail.com>
2022-08-21 22:13:03 +0300

    v3

--- interests/task.js
+++ interests/task.js
@@ -1,29 +1,28 @@
 function findInterest(){
-    const box = document.querySelectorAll('.interest')
+    const box = document.querySelectorAll('.interest__check')
+
     for (let i = 0; i < box.length; i++){
         box[i].addEventListener('change', () => {
-            console.log(box[i])
-            if (box[i]['firstElementChild']['firstElementChild'].checked){
-                console.log('if')
-                switchBoxis(box[i], true);
-                return false
+            //console.log(box[i].parentNode.parentNode.parentNode.className=='')
+            console.log(box[i].parentNode.parentNode)
+            if (box[i].checked){
+                switchBoxis(box[i].parentNode.parentNode, true);
+                return false;
+            } else {
+                switchBoxis(box[i].parentNode.parentNode, false);
+                return false;
             }
-            else {
-                console.log('else')
-                switchBoxis(box[i], false);   
-            }  
         })
     }
-    
 }
 
-//Прошу объяснить, почему вложенные чекбоксы не проставляются а цикл срабатывает на условие else
 
 function switchBoxis(elem, bool){
     const boxis = elem.querySelectorAll('.interest__check');
     for (let i =0; i < boxis.length;i++){
         boxis[i].checked = bool;
     }
+    return false
 }
 
 findInterest()
